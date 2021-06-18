a8b45c783d0e5cf14d67520391543f14d6d9a6a8
Matt Linder <mjlinder218@gmail.com>
2021-06-18 09:50:50 -0700

    Added remaining view-all query functions and added functions to add and remove an employee. Also made other minor changes.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,11 +15,23 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   initialQuestions();
+  listsFromDb();
 });
 
-const runSearch = () => {
-  console.log("Service started");
-};
+const listsFromDb = () => {
+  connection.query("SELECT * from employee", function(error, res) {
+    employeesInDb = res.map(employee => ({
+      name: `${employee.first_name} ${employee.last_name}`,
+      value: employee.id
+    }));
+  });
+  connection.query("SELECT * from department", function(error, res) {
+    departmentsInDb = res.map(dept => ({ name: dept.name, value: dept.id }));
+  });
+  connection.query("SELECT * from role", function(error, res) {
+    rolesInDb = res.map(role => ({ name: role.title, value: role.id }));
+  });
+}
 
 const initialQuestions = () => {
   inquirer
@@ -28,8 +41,6 @@ const initialQuestions = () => {
       message: 'What would you like to do?',
       choices: [
         'View all employees',
-        'View all employees by department',
-        'View all employees by manager',
         'Add employee',
         'Remove employee',
         'Update employee role',
@@ -48,35 +59,140 @@ const initialQuestions = () => {
         case 'View all employees':
           viewAllEmployees();
           break;
-
-        case 'View all employees by department':
-          viewAllEmployeesByDepartment();
+        case 'Add employee':
+          addEmployee();
           break;
-
-        case 'View all employees by manager':
-          viewAllEmployeesByManager();
+        case 'Remove employee':
+          removeEmployee();
           break;
-
-// Add remaining cases
-
+        case 'View departments':
+          viewDepartments();
+          break;
+        case 'View roles':
+          viewRoles();
+          break;
         default:
           console.log(`Invalid action: ${answer.action}`);
           break;
       }
     });
+};
 
-  };
+const viewAllEmployees = () => {
+  const query = 
+    'SELECT * FROM employee';
+  connection.query(query, (err, res) => {
+    res.forEach(({ id, first_name, last_name, role_id, manager_id }) => console.table(res));
+    initialQuestions();
+  });
+};
 
+const viewDepartments = () => {
+  const query = 
+    'SELECT * FROM department';
+  connection.query(query, (err, res) => {
+    res.forEach(({ id, name }) => console.table(res));
+    initialQuestions();
+  });
+};
 
-  const viewAllEmployees = () => {
-    const query = 'SELECT * FROM employee';
-    connection.query(query, [], (err, res) => {
-      res.forEach(({ id, first_name, last_name, role_id, manager_id }) => {
-        console.log(
-          `ID: ${id} || First name: ${first_name} || Last name: ${last_name} || Role ID: ${role_id} || Manager ID: ${manager_id}`
-        );
-      });
-      initialQuestions();
+const viewRoles = () => {
+  const query = 
+    'SELECT * FROM role';
+  connection.query(query, (err, res) => {
+    res.forEach(({ id, title, salary, department_id }) => console.table(res));
+    initialQuestions();
+  });
+};
+
+const addEmployee = () => {
+  listsFromDb();
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Employee id: '
+      },        {
+        type: 'input',
+        name: 'first_name',
+        message: 'First name: '
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'Last name: '
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: 'Role: ',
+        choices: rolesInDb
+      },
+      {
+        type: 'input',
+        name: 'manager_id',
+        message: 'Manager ID: '
+      }
+    ])
+    // .then((answer) => {
+    //   const query = connection.query(
+    //     "INSERT INTO employee SET ?",
+    //     {
+    //       id: answer.id,
+    //       first_name: answer.first_name,
+    //       last_name: answer.last_name,
+    //       role_id: answer.role,
+    //       manager_id: answer.manager_id
+    //     },
+    //     function(err, res) {
+    //       if (err) throw err;  // *****check pattern
+    //       console.log(query);
+    //       console.log('Employee added');
+    //       initialQuestions();
+    //     }
+    //   );
+    // });
+
+    .then((answer) => {
+      const query = connection.query(
+        'INSERT INTO employee SET ?', 
+        { 
+          id: answer.id,
+          first_name: answer.first_name,
+          last_name: answer.last_name,
+          role_id: answer.role,
+          manager_id: answer.manager_id   // ****make optional???
+        },
+         (err, res) => {
+          console.log(query);  // *******
+          console.log('Employee deleted');
+          initialQuestions();
+        }
+      );
     });
-     
-  };
+
+
+}
+
+const removeEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'id',
+        message: 'ID of employee to delete: '
+      }
+    ])
+    .then((answer) => {
+      const query = connection.query(
+        'DELETE FROM employee WHERE ?', { id: answer.id }, (err, res) => {
+          console.log(query);   // **************
+          console.log('Employee deleted');
+          initialQuestions();
+        }
+      );
+    });
+}
+
+
